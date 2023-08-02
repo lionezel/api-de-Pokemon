2acdd0ae0f3b0ab3d3e5702c8d2228950dec48dc
lionezel <lionezelfts@gmail.com>
2023-08-01 19:07:48 -0500

    detalle de pokemones v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
 import { FiltroPipe } from './pipe/filtro.pipe';
 import { DetailPokemonComponent } from './page/detail-pokemon/detail-pokemon.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FiltroPipe, DetailPokemonComponent],
+  declarations: [AppComponent, HomeComponent, FiltroPipe, DetailPokemonComponent, ProgressBarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
